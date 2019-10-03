295ac82e487f5ace8c45a8a77bfcd81e83aa5348
Vitali Matusevich <matvi@mail.ru>
2019-10-03 20:34:11 +0300

    fix: task ok

--- src/index.js
+++ src/index.js
@@ -6,34 +6,26 @@
  */
 
 module.exports = function getTimeForEducation(
-// function getTimeForEducation(
-
-    focus = 'family', 
+    focus = 'family',
     knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      
-      const totalHours = (knowsProgramming == 'true') ? 500 : 800;
-      let weeks = 0;
-      
-      if (focus == "family") weeks = totalHours / config.family;
-      
-      if (focus == "friends") weeks = totalHours / config.friends;
+    config = { family: 4 }
+) {
 
-      if (focus == "normal_life") weeks = totalHours / config.normal_life;
-
-      if (focus == "profession") weeks = totalHours / config.profession;
-
-      if (focus == "carrier") weeks = totalHours / config.carrier;
-
-      if (focus == "top_peformance") weeks = totalHours / config.top_peformance;
-     
-      // TODO: сделать округление weeks в большую сторону;
-
-      return Math.ceil(weeks);
-  };
+    let totalHours = (knowsProgramming == true) ? 800 : 1300;
+    let weeks = 0;
 
 
-// getTimeForEducation();
+    if (focus == "family") weeks = totalHours / config.family;
 
-  
+    if (focus == "friends") weeks = totalHours / config.friends;
+
+    if (focus == "normal_life") weeks = totalHours / config.normal_life;
+
+    if (focus == "profession") weeks = totalHours / config.profession;
+
+    if (focus == "carrier") weeks = totalHours / config.carrier;
+
+    if (focus == "top_peformance") weeks = totalHours / config.top_peformance;
+
+    return Math.ceil(weeks);
+};
